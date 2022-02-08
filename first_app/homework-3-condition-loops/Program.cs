685d0da2f45ee0b297ce010906e0efb476d8372f
andrey-polchanov <polchanov@gmail.com>
2022-02-08 10:21:11 +0200

    homework-3-condition-loops

---
+++ first_app/homework-3-condition-loops/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace homework_3_condition_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message1 = Console.ReadLine();
+            string message2 = Console.ReadLine();
+            if (float.TryParse(message1, out float x))
+            {     
+                   
+                    if (float.TryParse(message2, out float y))
+                    {
+                        if (x == y) { Console.WriteLine(x); }
+                        else
+                        {
+                            float min = Math.Min(x, y);
+                            float max = Math.Max(x, y);
+                            float result = 0;
+                            for (; min <= max ; min++)
+                            {
+                                result += min;
+                            }
+                            Console.WriteLine(result);
+                        }
+                    }
+                    else { Console.WriteLine("Invalid input"); }
+            }
+            else { Console.WriteLine("Invalid input"); }
+
+
+
+
+
+        }
+    }
+}
